1e0a82793e2b38b36e5b6f3875e4eafa5658da88
waitback0414 <awesomefactory.kobe@gmail.com>
2025-05-05 23:14:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,4 @@ st.write("made by AWESOME FACTORY")
 
 st.markdown("""## This is a markdown
 
-# ### Streamlit is awesome!
-# マークダウンでかけて便利！
-# - リスト
-# > 引用
 
-# ```python
-# import streamlit as st
-# st.title("This is a title")
-# st.write("Hello World!")
-# ``` 
-# """)
