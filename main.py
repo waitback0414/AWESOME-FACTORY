8f20cf212be791a8ed4d53773d6e45e91f920c30
waitback0414 <awesomefactory.kobe@gmail.com>
2025-05-05 23:54:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 
 st.set_page_config(page_title="Streamlit App", page_icon=":shark:")
 
@@ -6,9 +7,9 @@ st.title("E-DOYUの回答率集計アプリ")
 st.write("made by AWESOME FACTORY")
 
 # 変換後の画像 URL
-image_url = "https://drive.google.com/uc?export=view&id=/1t9PTUy89tBejKogE0ywbEkO75FEDdlpc"
+image = Image.open("https://drive.google.com/uc?export=view&id=/1t9PTUy89tBejKogE0ywbEkO75FEDdlpc")
 
 # 画像を表示
 # 修正後（推奨）
-st.image(image_url, use_container_width=True)
+st.image(image, use_container_width=True)
 
