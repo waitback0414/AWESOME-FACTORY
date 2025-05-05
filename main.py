a5d3b53b2c632368881753f3e5a16f1082b03815
waitback0414 <awesomefactory.kobe@gmail.com>
2025-05-05 23:51:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.title("E-DOYUの回答率集計アプリ")
 st.write("made by AWESOME FACTORY")
 
 # 変換後の画像 URL
-image_url = "https://drive.google.com/file/d/1t9PTUy89tBejKogE0ywbEkO75FEDdlpc"
+image_url = "https://drive.google.com/uc?export=view&id=/1t9PTUy89tBejKogE0ywbEkO75FEDdlpc"
 
 # 画像を表示
 # 修正後（推奨）
