f8264416bb255259262d232eb62c6ee136026b85
waitback0414 <awesomefactory.kobe@gmail.com>
2025-05-05 23:13:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import streamlit as st
 
 st.set_page_config(page_title="Streamlit App", page_icon=":shark:")
 
-st.title("This is a title")
-st.write("Hello World!")
+st.title("E-DOYUの回答率集計アプリ")
+st.write("made by AWESOME FACTORY")
 
 st.markdown("""## This is a markdown
 
-### Streamlit is awesome!
-マークダウンでかけて便利！
-- リスト
-> 引用
+# ### Streamlit is awesome!
+# マークダウンでかけて便利！
+# - リスト
+# > 引用
 
-```python
-import streamlit as st
-st.title("This is a title")
-st.write("Hello World!")
-``` 
-""")
+# ```python
+# import streamlit as st
+# st.title("This is a title")
+# st.write("Hello World!")
+# ``` 
+# """)
